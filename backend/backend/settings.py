c9466518ed4e46af4a7f19515cec9aa2c490e9ea
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-07-20 04:49:25 +0300

    22222
    2222

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,8 +62,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # Меняем настройку Django: теперь для работы будет использоваться
-        # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
@@ -113,6 +111,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
