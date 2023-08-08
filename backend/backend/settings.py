dd73ed14fc81e014df7311db046d1145e46c996b
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-08-08 08:53:28 +0300

    444444
    444444

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.70.95', '127.0.0.1', 'localhost', 'sprt14kitty.ddns.net']
-
+ALLOWED_HOSTS = ['158.160.28.33','127.0.0.1','alex86kittygram444.ddns.net']
 
 # Application definition
 
