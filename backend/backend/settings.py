f8388195bbb7178fc4b9fc1ffb177639f7630467
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-09-03 03:05:36 +0300

    gzzsd
    sgdgsh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mysecretpassword'),
         'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
